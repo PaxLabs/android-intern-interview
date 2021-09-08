3f27debb4bcb1b7bf1afeae2b7f6ca7754e618eb
Rebecca Stecker <rstecker@pax.com>
2021-09-07 17:51:08 -0700

    cleaning up project

---
+++ PaxAndroidInterview1/app/src/main/java/com/example/paxandroidinterview/BookAdapter.java
@@ -0,0 +1,49 @@
+package com.example.paxandroidinterview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
+
+    private List<WeirdBook> localDataSet;
+
+    public static class BookViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public BookViewHolder(View view) {
+            super(view);
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+        public void bind(String title) {
+            textView.setText(title);
+        }
+    }
+
+    public BookAdapter() {
+        localDataSet = new ArrayList<>();
+    }
+
+    @Override public BookViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.viewholder_book, viewGroup, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override public void onBindViewHolder(BookViewHolder viewHolder, final int position) {
+        viewHolder.bind(localDataSet.get(position).getTitle());
+    }
+
+    @Override public int getItemCount() {
+        return localDataSet.size();
+    }
+
+    public void updateBooks(List<WeirdBook> newBooks) {
+        localDataSet = newBooks;
+        notifyDataSetChanged();
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# android-intern-interview
+# android-interview
 
 For the interview, candidates are expected to have Android Studio set up on their machine with this project checked out and able to compile.
 
 Zoom will be used for video conferencing and **the candidate is expected to screen share/show their Android Studio application window during the coding exercise portion of the interview.**
 
-Due to emulators being lame and Zoom being finicky the candidate will not be expected to run/launch the sample app during the interview, but "does it compile" will be checked/used. It would of course be useful to launch the application ahead of time to just be aware of what it is.
+Candidates are expected to run the application locally (emulator or phone) during the interview to verify progress but due to emulators being lame and Zoom being finicky the candidate will not be expected to screen-share the sample app during the interview. The question of whether it compiles will be checked/used however. Candidates are welcome to launch the application ahead of time to just be aware of what it is.
 
 Some Android libraries that will be touched upon/referenced: `RecyclerView`, `Fragment`, `AppCompatActivity`, `ViewModel` 
